74e9ac8a0dbc31cec0bbd8d0c865a1ec9382cbea
unknown <rodrigo_fnl@hotmail.com>
2020-05-24 17:42:22 -0300

    configurações

---
+++ src/rest/ParticiparRest.java
@@ -0,0 +1,42 @@
+package rest;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("participar")
+public class ParticiparRest 
+{
+	@Path("html")
+	@POST
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String getHTML()
+	{
+		return "<h1>TEXTO HTML</h1>";
+	}
+	
+	@Path("text")
+	@POST
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getText()
+	{
+		return "TEXTO";
+	}
+		
+	@Path("json")
+	@POST
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public String getJSON()
+	{
+		JsonObject object = Json.createObjectBuilder().add("josn", "Json message").add("message", "mensagem do texto json").build();			
+		return object.toString();
+	}
+
+}
