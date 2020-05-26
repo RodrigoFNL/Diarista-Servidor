6551a79635e39d8a2d01a0cddda60bb8407e1f1d
unknown <rodrigo_fnl@hotmail.com>
2020-05-26 10:07:17 -0300

    pom

--- src/conf/FilterRest.java
+++ src/conf/FilterRest.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -68,4 +69,17 @@ public class FilterRest implements Filter
 			return;
 		}
 	}
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException
+	{		
+		
+	}
+
+	@Override
+	public void destroy()
+	{
+				
+	}
+	
 }
