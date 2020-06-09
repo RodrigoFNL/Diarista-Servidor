9b8b51b186462d195ebcf8e5d30c67a30654537c
unknown <rodrigo_fnl@hotmail.com>
2020-06-09 09:47:04 -0300

    email

--- src/conf/EmailInfo.java
+++ src/conf/EmailInfo.java
@@ -4,6 +4,7 @@ public class EmailInfo
 {
 	public final static String EMAIL_ADMINISTRADOR 			="definir@um.email.admistrativo";	
 	public static final String EMAIL_NAO_RESPONDA 			="diarista.nao.responda@gmail.com";
+	public static final String USERNAME_NAO_RESPONDA 		="diarista.nao.responda@gmail.com";
 	public static final String PASSWORD_EMAIL_NAO_RESPONDA 	="diarista@2020#";
 	
 	public static final String SERVIDOR_SMTP = "smtp.gmail.com";
--- src/util/EmailUtil.java
+++ src/util/EmailUtil.java
@@ -1,21 +1,12 @@
 package util;
 
-import java.io.StringWriter;
-import java.io.Writer;
-import java.util.Date;
 import java.util.Properties;
-
-import javax.mail.Address;
-import javax.mail.Authenticator;
-import javax.mail.BodyPart;
 import javax.mail.Message;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeMessage.RecipientType;
 
 import conf.EmailInfo;
 
@@ -28,55 +19,90 @@ public class EmailUtil
 		sendEmail(email, "Número do Cupon", coupon);
 	}
 	public static void sendEmail(String email, String title, String text)
-	{
-		try 
+	{	
+		String to = email;	
+		String from = EmailInfo.EMAIL_NAO_RESPONDA;
+		final String username = EmailInfo.USERNAME_NAO_RESPONDA;
+		final String password = EmailInfo.PASSWORD_EMAIL_NAO_RESPONDA;
+		String host = EmailInfo.SERVIDOR_SMTP;
+		
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", host);
+		props.put("mail.smtp.port", "587");
+		
+		Session session = Session.getInstance(props, new javax.mail.Authenticator() 
 		{
-			//INSERE AS PROPRIEDADES DO EMAIL, COMO LOGIN, SENHA, SMTP.
-			Properties props = new Properties();
-
-			props.put("mail.from", EmailInfo.EMAIL_NAO_RESPONDA);
-			props.setProperty("mail.smtp.user", EmailInfo.EMAIL_NAO_RESPONDA);
-			props.setProperty("mail.smtp.password", EmailInfo.PASSWORD_EMAIL_NAO_RESPONDA);
-
-			props.put("mail.smtp.host", EmailInfo.SERVIDOR_SMTP);
-			props.put("mail.smtp.starttls.enable", "true");
-			props.setProperty("mail.smtp.auth", "true");
-
-
-			Writer out = new StringWriter();
-
-			BodyPart body = new MimeBodyPart();					
-			body.setContent(out.toString(), "text/html; charset=utf-8");			
-
-			Session session = Session.getInstance(props, new Authenticator() 
+			protected PasswordAuthentication getPasswordAuthentication() 
 			{
-				public PasswordAuthentication getPasswordAuthentication() 
-				{
-					return new PasswordAuthentication(EmailInfo.EMAIL_NAO_RESPONDA, EmailInfo.PASSWORD_EMAIL_NAO_RESPONDA);
-				}
-			});
-
-			Message msg = new MimeMessage(session);			
-
-			Address[] adrAddress = new Address[1];
-			adrAddress[0] = new InternetAddress(email);
-
-			msg.setRecipients(RecipientType.TO, adrAddress);
-
-			msg.setFrom(new InternetAddress(EmailInfo.EMAIL_NAO_RESPONDA));
-			msg.setSubject(title);
-			msg.setSentDate(new Date());
-			msg.setContent(text, "text/html; charset=utf-8");
-
-			Transport  transport = session.getTransport("smtp");
-			transport.connect(EmailInfo.SERVIDOR_SMTP, EmailInfo.EMAIL_NAO_RESPONDA, EmailInfo.PASSWORD_EMAIL_NAO_RESPONDA);
-			Transport.send(msg);
-			transport.close();
-
-		} 
-		catch (Exception e) 
+				return new PasswordAuthentication(username, password);
+			}
+		});
+		try
 		{
-			e.printStackTrace();			
-		}	
+		
+			Message message = new MimeMessage(session);		
+			message.setFrom(new InternetAddress(from));	
+			message.setRecipients(Message.RecipientType.TO,	InternetAddress.parse(to));	
+			message.setSubject(title);	
+			message.setText(text);	
+			Transport.send(message);
+		} 
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+
+		//		try 
+		//		{
+		//			//INSERE AS PROPRIEDADES DO EMAIL, COMO LOGIN, SENHA, SMTP.
+		//			Properties props = new Properties();
+		//
+		//			props.put("mail.from", EmailInfo.EMAIL_NAO_RESPONDA);
+		//			props.setProperty("mail.smtp.user", EmailInfo.EMAIL_NAO_RESPONDA);
+		//			props.setProperty("mail.smtp.password", EmailInfo.PASSWORD_EMAIL_NAO_RESPONDA);
+		//
+		//			props.put("mail.smtp.host", EmailInfo.SERVIDOR_SMTP);
+		//			props.put("mail.smtp.port", "25");
+		//			props.put("mail.smtp.starttls.enable", "true");
+		//			props.setProperty("mail.smtp.auth", "true");			
+		//		
+		//
+		//			Writer out = new StringWriter();
+		//
+		//			BodyPart body = new MimeBodyPart();					
+		//			body.setContent(out.toString(), "text/html; charset=utf-8");			
+		//
+		//			Session session = Session.getInstance(props, new Authenticator() 
+		//			{
+		//				public PasswordAuthentication getPasswordAuthentication() 
+		//				{
+		//					return new PasswordAuthentication(EmailInfo.EMAIL_NAO_RESPONDA, EmailInfo.PASSWORD_EMAIL_NAO_RESPONDA);
+		//				}				
+		//			});
+		//
+		//			Message msg = new MimeMessage(session);			
+		//
+		//			Address[] adrAddress = new Address[1];
+		//			adrAddress[0] = new InternetAddress(email);
+		//
+		//			msg.setRecipients(RecipientType.TO, adrAddress);
+		//
+		//			msg.setFrom(new InternetAddress(EmailInfo.EMAIL_NAO_RESPONDA));
+		//			msg.setSubject(title);
+		//			msg.setSentDate(new Date());
+		//			msg.setContent(text, "text/html; charset=utf-8");
+		//
+		//			Transport  transport = session.getTransport("smtp");		
+		//			transport.connect();
+		//			Transport.send(msg);
+		//			transport.close();			
+		//
+		//		} 
+		//		catch (Exception e) 
+		//		{
+		//			e.printStackTrace();			
+		//		}			
 	}
 }
