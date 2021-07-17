9a3260358531b0415029642a4e50dd8cdf883051
unknown <rodrigo_fnl@hotmail.com>
2021-07-17 17:34:38 -0300

    task

--- src/main/java/br/com/diarista/geral/rest/ModelRest.java
+++ src/main/java/br/com/diarista/geral/rest/ModelRest.java
@@ -1,15 +1,21 @@
 package br.com.diarista.geral.rest;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.google.gson.Gson;
 
 @RestController
 @RequestMapping("/rest/model")
 public class ModelRest 
 {
+	private int x = 0;
+	
 	@RequestMapping("/html")
 	public String getHTML()
 	{
@@ -23,9 +29,43 @@ public class ModelRest
 	}	
 	
 	@RequestMapping("/json")
-	public List<Object> getJson()
+	public String getJson()
 	{
-		List<Object> lista = new ArrayList<Object>();
-		return lista;
+		try
+		{	
+			Timer timer = new Timer(); 
+			timer.scheduleAtFixedRate(new TimerTask() 
+			{
+				@Override 
+				public void run()
+				{ 
+					try
+					{		
+						x++;
+						URL url = new URL("https://www.casadoscontos.com.br/texto/202103899");	
+						url.openConnection();				
+						InputStream is = url.openStream();		
+						String response = new String(is.readAllBytes(), "UTF-8");	
+						
+						System.out.println("=================================================");
+						System.out.println(x);
+						System.out.println("=================================================");
+						
+						System.out.println(response);
+					}
+					catch (Exception e)
+					{			
+						System.out.println("error");
+					}					
+				}		    
+			}, 1000, 1000);			
+		
+			return "ok";
+		}
+		catch (Exception e)
+		{	
+			Gson gson = new Gson();			
+			return gson.toJson(e);	
+		}	
 	}	
 }
